5d0f62a48f4e64112ef17628df60fc43ecca3366
Ori Korngut <ori.korngut@gmail.com>
2024-06-03 19:36:45 +0300

    Initial commit

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -6,20 +6,20 @@ model = YOLO('best Keller.pt')
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
-    print("Error: Could not open webcam.")
+    print("Error")
     exit()
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Error: Could not read frame.")
+        print("Error")
         break
 
     results = model(frame)
 
     annotated_frame = results[0].plot()
 
-    cv2.imshow("YOLOv8 Real-Time Object Detection", annotated_frame)
+    cv2.imshow("Detect", annotated_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
